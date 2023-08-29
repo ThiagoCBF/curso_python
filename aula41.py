f7a8b00ba47461902c4aed67349054ffb4ad224a
Thiago Ferreira <thiagocbf@outlook.com>
2023-08-29 14:20:47 -0300

    While/Else
    Else no While

---
+++ aula41.py
@@ -0,0 +1,17 @@
+# While/else
+
+string = 'ABCDEFG IJKL' 
+
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('Não encontrei um espaço na string')
+print('Fora do while')
