c3d22da64da4d5e78fd49391972af3a16795f9b4
Thiago Ferreira <thiagocbf@outlook.com>
2023-09-05 17:08:35 -0300

    Desempacotamento em chamadas

---
+++ aula60.py
@@ -0,0 +1,10 @@
+# Desempacotamento em chamadas 
+# de métodos e funções 
+
+string = 'ABCD'
+lista = ['Thiago', 'Couto', 1, 2, 3, 'Ferreira']
+tupla = 'Python', 'é', 'legal'
+# p, b, *_, ap, u = lista
+# print(p, u, ap)
+print(lista)
+print(*lista)
