c79f74d615f0083c7153d84deb4a823428df9481
Thiago Ferreira <thiagocbf@outlook.com>
2023-09-11 16:56:53 -0300

    Return Python
    Return - retorna o valor fazendo ser utilizado

---
+++ aula68.py
@@ -0,0 +1,15 @@
+'''
+Retorno de valores das Funções (return)
+'''
+variavel = print('Thiago')
+
+def soma(x, y):
+    if x > 10:
+        return 10
+    return x + y
+
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+print(soma1)
+print(soma2)
+print(soma(10, 50))
